820261c7af7cb5b2cf46607d36b3b199749aa723
Said Mariano <smariano170@gmail.com>
2025-11-26 17:25:12 -0600

    feat: Consumo de API

---
+++ ETL/extract.py
@@ -0,0 +1,18 @@
+import requests
+
+url = 'https://api.jikan.moe/v4/anime'
+
+try:
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        print('Petición exitosa')
+        data = response.json()
+        lista_data = data['data']
+        print(lista_data)
+    else:
+        print(f'Error en la petición \nEstado: {response.status_code}')
+        print(response.text)
+
+except requests.exceptions.RequestException as e:
+    print(f'Error de conexión: {e}')
