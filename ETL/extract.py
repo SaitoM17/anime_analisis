4be88df0494edfb82e057561ea3b5e5290f02c11
Said Mariano <smariano170@gmail.com>
2025-11-27 13:49:14 -0600

    faet: Prueba de extracción de datos especificos de varias paginas

--- ETL/extract.py
+++ ETL/extract.py
@@ -5,27 +5,28 @@ anime_dic = {
     'title': []
 }
 
-url = 'https://api.jikan.moe/v4/anime'
+for pagina in range(1,3):
+    url = f'https://api.jikan.moe/v4/anime?page={pagina}'
 
-try:
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        print('Petición exitosa')
-        data = response.json()
-        lista_data = data.get('data',[])
+    try:
+        response = requests.get(url)
         
-        for anime in lista_data:
-            mal_id = anime.get('mal_id')
-            title_ingles = anime.get('title_english')
+        if response.status_code == 200:
+            print('Petición exitosa')
+            data = response.json()
+            lista_data = data.get('data',[])
             
-            anime_dic['mal_id'].append(mal_id)
-            anime_dic['title'].append(title_ingles)
-    else:
-        print(f'Error en la petición \nEstado: {response.status_code}')
-        print(response.text)
+            for anime in lista_data:
+                mal_id = anime.get('mal_id')
+                title_ingles = anime.get('title_english')
+                
+                anime_dic['mal_id'].append(mal_id)
+                anime_dic['title'].append(title_ingles)
+        else:
+            print(f'Error en la petición \nEstado: {response.status_code}')
+            print(response.text)
 
-except requests.exceptions.RequestException as e:
-    print(f'Error de conexión: {e}')
+    except requests.exceptions.RequestException as e:
+        print(f'Error de conexión: {e}')
 
 print(anime_dic)
