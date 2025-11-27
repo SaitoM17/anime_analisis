0c3c5343f01f4108af215b6955b0aaa4fe336765
Said Mariano <smariano170@gmail.com>
2025-11-27 15:59:37 -0600

    refactor: Modificación al modo de obtener los datos

--- ETL/extract.py
+++ ETL/extract.py
@@ -2,7 +2,17 @@ import requests
 
 anime_dic = {
     'mal_id': [],
-    'title': []
+    'titulo': [],
+    'titulo_ingles': [],
+    'titulo_japones': [],
+    'tipo': [],
+    'episodios': [],
+    'annio': [],
+    'temporada': [],
+    'clasifificacion': [],
+    'duracion': [],
+    'sipnosis': [],
+    'anime_rank': []
 }
 
 for pagina in range(1,3):
@@ -17,11 +27,18 @@ for pagina in range(1,3):
             lista_data = data.get('data',[])
             
             for anime in lista_data:
-                mal_id = anime.get('mal_id')
-                title_ingles = anime.get('title_english')
-                
-                anime_dic['mal_id'].append(mal_id)
-                anime_dic['title'].append(title_ingles)
+                anime_dic['mal_id'].append(anime.get('mal_id'))
+                anime_dic['titulo'].append(anime.get('title'))
+                anime_dic['titulo_ingles'].append(anime.get('title_english'))
+                anime_dic['titulo_ingles'].append(anime.get('title_japanese'))
+                anime_dic['tipo'].append(anime.get('type'))
+                anime_dic['episodios'].append(anime.get('episodes'))
+                anime_dic['temporada'].append(anime.get('season'))
+                anime_dic['clasifificacion'].append(anime.get('rating'))
+                anime_dic['duracion'].append(anime.get('duration'))
+                anime_dic['sipnosis'].append(anime.get('synopsis'))
+                anime_dic['anime_rank'].append(anime.get('rank'))
+    
         else:
             print(f'Error en la petición \nEstado: {response.status_code}')
             print(response.text)
