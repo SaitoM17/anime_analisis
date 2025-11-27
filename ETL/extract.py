5f44c78aaf29888ba25dee211c7033a015f5cb80
Said Mariano <smariano170@gmail.com>
2025-11-27 13:16:07 -0600

    feat: Prueba de extracción de datos especificos

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://api.jikan.moe/v4/producers'
+url = 'https://api.jikan.moe/v4/anime'
 
 try:
     response = requests.get(url)
@@ -8,8 +8,14 @@ try:
     if response.status_code == 200:
         print('Petición exitosa')
         data = response.json()
-        lista_data = data['data']
-        print(lista_data)
+        lista_data = data.get('data',[])
+        # print(lista_data)
+        for anime in lista_data:
+            mal_id = anime.get('mal_id')
+            title_ingles = anime.get('title_english')
+            print(f"Título: {mal_id}")
+            print(f"Año: {title_ingles}")
+            print("---") # Separador para claridad
     else:
         print(f'Error en la petición \nEstado: {response.status_code}')
         print(response.text)
