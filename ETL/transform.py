fb188da793b95eb21be2d543c931d4fa514de7ec
Said Mariano <smariano170@gmail.com>
2025-11-28 18:04:00 -0600

    refactor: Cambio en código para pedir la cantidad de paginas a extrer información

--- ETL/transform.py
+++ ETL/transform.py
@@ -1,21 +1,6 @@
 import pandas as pd
 import extract 
 
-anime_raw = extract.extraccion_anime()
-genero_raw = extract.extraccion_genero()
-estudio_raw = extract.extraccion_studios()
-popularidad_raw = extract.extraccion_popularidad()
-anime_generos_raw = extract.extraccion_anime_genero()
-anime_studios_raw = extract.extraccion_anime_studio()
-
-lista_raw = [pd.DataFrame(anime_raw),
-             pd.DataFrame(genero_raw),
-             pd.DataFrame(estudio_raw),
-             pd.DataFrame(popularidad_raw),
-             pd.DataFrame(anime_generos_raw),
-             pd.DataFrame(anime_studios_raw)
-             ]
-
 def imputar_df(lista_df: list[pd.DataFrame]) -> list[pd.DataFrame]:
 
     for idx, df in enumerate(lista_df):
@@ -37,5 +22,22 @@ def imputar_df(lista_df: list[pd.DataFrame]) -> list[pd.DataFrame]:
                 
                 else:
                     print(f"Columna '{columna}' contiene nulos pero no es numérica ni de texto; se omitió.")
+
+
+anime_raw = extract.extraccion_anime(3)
+genero_raw = extract.extraccion_genero()
+estudio_raw = extract.extraccion_studios(3)
+popularidad_raw = extract.extraccion_popularidad(3)
+anime_generos_raw = extract.extraccion_anime_genero(3)
+anime_studios_raw = extract.extraccion_anime_studio(3)
+
+lista_raw = [pd.DataFrame(anime_raw),
+             pd.DataFrame(genero_raw),
+             pd.DataFrame(estudio_raw),
+             pd.DataFrame(popularidad_raw),
+             pd.DataFrame(anime_generos_raw),
+             pd.DataFrame(anime_studios_raw)
+             ]
+                    
 
 imputar_df(lista_raw)
