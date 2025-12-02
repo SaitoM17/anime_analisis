4f3ef1037c7c4ab31e84d6da8d4f6a99b2a8ab81
Said Mariano <smariano170@gmail.com>
2025-12-02 10:50:46 -0600

    feat: Función de cada de tabla animes

--- ETL/load.py
+++ ETL/load.py
@@ -1,28 +1,54 @@
 import mysql.connector
 
+def load_animes(df):
+    connection = None
 
-connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="saito17Jr",
+            database="anime"
+        )
 
-try:
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="saito17Jr",
-        database="anime"
-    )
+        if connection.is_connected():
+            db_info = connection.get_server_info()
+            print(f'Conexión exitosa a MySQL version:{db_info}')
+            print('Conectado a la base de datos')
 
-    if connection.is_connected():
-        db_info = connection.get_server_info()
-        print(f'Conexión exitosa a MySQL version:{db_info}')
-        print('Conectado a la base de datos')
+            cursor = connection.cursor()
 
-        cursor = connection.cursor()
-        cursor.execute("SELECT DATABASE()")
-        db_name = cursor.fetchone()[0]
-        print(f'Base de datos seleccionada {db_name}')
-except mysql.connector.Error as e:
-    print(f'Error al conectar a MySQL {e}')
-finally:
-    if connection is not None and connection.is_connected():
-        connection.close()
-        print('Conexión cerrada')
+            insert_query = """
+            INSERT INTO anime(
+                mal_id, titulo, titulo_english, titulo_japanese, tipo, episodios, annio, temporada, clasificacion, duracion, sinopsis, anime_rank
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                titulo = VALUES(titulo),
+                titulo_ingles = (titulo_ingles),
+                titulo_japones = (titulo_japones),
+                tipo = (tipo),
+                episodios = (episodios),
+                annio = (annio),
+                temporada = (temporada),
+                clasificacion = (clasificacion),
+                duracion = (duracion),
+                sinopsis = (sinopsis),
+                anime_rank = (anime_rank)
+            """
+
+            for _, row in df.iterrows():
+                cursor.execute(insert_query, tuple(row))
+
+            connection.commit()
+            print(f'Datos de la tabla anime insertados/actualizados con éxito ({df.shape[0]} filas).')
+
+    except mysql.connector.Error as e:
+        print(f'Error al conectar a MySQL {e}')
+        if connection and connection.is_connected():
+            connection.rollback()
+            print('Rollback ejecutado.')
+    finally:
+        if connection is not None and connection.is_connected():
+            connection.close()
+            print('Conexión cerrada')
