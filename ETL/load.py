1195444157279f3fccbea5e75a9d09445fa7e588
Said Mariano <smariano170@gmail.com>
2025-12-02 11:12:41 -0600

    feat: Función de carga de tabla popularidad

--- ETL/load.py
+++ ETL/load.py
@@ -140,3 +140,50 @@ def load_estudios(df):
         if connection is not None and connection.is_connected():
             connection.close()
             print('Conexión cerrada')
+
+def load_popularidad(df):
+    connection = None
+
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="saito17Jr",
+            database="anime"
+        )
+
+        if connection.is_connected():
+            db_info = connection.get_server_info()
+            print(f'Conexión exitosa a MySQL version:{db_info}')
+            print('Conectado a la base de datos')
+
+            cursor = connection.cursor()
+
+            insert_query = """
+            INSERT INTO genros(
+                mal_id, score, score_by, miembros, favoritos, popularidad
+            )
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                score = VALUES(score),
+                score_by = VALUES(score_by),
+                miembros = VALUES(miembros),
+                favoritos = VALUES(favoritos),
+                popularidad = VALUES(popularidad)
+            """
+
+            for _, row in df.iterrows():
+                cursor.execute(insert_query, tuple(row))
+
+            connection.commit()
+            print(f'Datos de la tabla anime insertados/actualizados con éxito ({df.shape[0]} filas).')
+
+    except mysql.connector.Error as e:
+        print(f'Error al conectar a MySQL {e}')
+        if connection and connection.is_connected():
+            connection.rollback()
+            print('Rollback ejecutado.')
+    finally:
+        if connection is not None and connection.is_connected():
+            connection.close()
+            print('Conexión cerrada')
