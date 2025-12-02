d09c7f724fee2e1ab2685056291a06dadf760e1a
Said Mariano <smariano170@gmail.com>
2025-12-02 11:04:47 -0600

    feat: Función de carga de tabla estudios

--- ETL/load.py
+++ ETL/load.py
@@ -95,3 +95,48 @@ def load_generos(df):
         if connection is not None and connection.is_connected():
             connection.close()
             print('Conexión cerrada')
+
+def load_estudios(df):
+    connection = None
+
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="saito17Jr",
+            database="anime"
+        )
+
+        if connection.is_connected():
+            db_info = connection.get_server_info()
+            print(f'Conexión exitosa a MySQL version:{db_info}')
+            print('Conectado a la base de datos')
+
+            cursor = connection.cursor()
+
+            insert_query = """
+            INSERT INTO genros(
+                estudio_id, nombre_estudio, favoritos, establecido
+            )
+            VALUES (%s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                nombre_estudio = VALUES(nombre_estudio),
+                favoritos = VALUES(favoritos),
+                establecido = VALUES(establecido)
+            """
+
+            for _, row in df.iterrows():
+                cursor.execute(insert_query, tuple(row))
+
+            connection.commit()
+            print(f'Datos de la tabla anime insertados/actualizados con éxito ({df.shape[0]} filas).')
+
+    except mysql.connector.Error as e:
+        print(f'Error al conectar a MySQL {e}')
+        if connection and connection.is_connected():
+            connection.rollback()
+            print('Rollback ejecutado.')
+    finally:
+        if connection is not None and connection.is_connected():
+            connection.close()
+            print('Conexión cerrada')
