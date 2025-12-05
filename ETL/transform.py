744a4d60fadcd1b054422c8e5eacacbfbc47fbdd
Said Mariano <smariano170@gmail.com>
2025-12-05 17:48:59 -0600

    refactore: Mejoramiento a la imputación de datos y tranformación de tipo de datos

--- ETL/transform.py
+++ ETL/transform.py
@@ -5,8 +5,15 @@ def imputar_df(lista_df: list[pd.DataFrame]) -> list[pd.DataFrame]:
 
     lista_df_limpios = []
 
-    for idx, df in enumerate(lista_df):
-        print(f'\n--- Limpiando DataFrame {idx + 1}/{len(lista_df)} (Columnas: {list(df.columns)}) ---')
+    for nombre_df, df in lista_df:
+        print(f'\n--- Limpiando DataFrame "{nombre_df}" (Columnas: {list(df.columns)}) ---')
+
+        if 'establecido' in df.columns:
+            df['establecido'] = pd.to_datetime(df['establecido'], errors='coerce')
+            df['establecido'] = df['establecido'].dt.strftime('%Y-%m-%d')
+            imputacion_fecha = '2261-12-31'
+            df['establecido'] = df['establecido'].fillna(imputacion_fecha).replace('NaT', imputacion_fecha)
+            print(f"Imputado 'establecido': Nulos (NaT) rellenados con '{imputacion_fecha}'")
 
         # Detectar valores nulos e imputarlos con 'N/A y '0
         for columna in df.columns:
@@ -19,8 +26,8 @@ def imputar_df(lista_df: list[pd.DataFrame]) -> list[pd.DataFrame]:
                 
                 # Verificar si el tipo de dato es NUMÉRICO (int, float, etc.)
                 elif pd.api.types.is_numeric_dtype(df[columna]):
-                    df[columna] = df[columna].fillna(0) #.astype(int) # Usar .astype(int) para asegurar enteros si aplica
-                    print(f"Imputado '{columna}': Nulos rellenados con 0")
+                    df[columna] = df[columna].fillna(9999) #.astype(int) # Usar .astype(int) para asegurar enteros si aplica
+                    print(f"Imputado '{columna}': Nulos rellenados con '9999'")
                 
                 else:
                     print(f"Columna '{columna}' contiene nulos pero no es numérica ni de texto; se omitió.")
@@ -34,78 +41,50 @@ def cambio_tipo_dato(lista_df):
     lista_df_tipo_correcto = []
     # Tabla anime
     tabla_anime = lista_df[0]
-    print(f'\n--- DataFrame {"Anime"} antes del cambio ---')
-    print(tabla_anime.info())
     tabla_anime['mal_id'] = tabla_anime['mal_id'].astype(int)
     tabla_anime['titulo'] = tabla_anime['titulo'].astype(object)
-    tabla_anime['titulo_ingles'] = tabla_anime['titulo_ingles'].astype(object)
-    tabla_anime['titulo_japones'] = tabla_anime['titulo_japones'].astype(object)
     tabla_anime['tipo'] = tabla_anime['tipo'].astype(object)
     tabla_anime['episodios'] = tabla_anime['episodios'].astype(int)
     tabla_anime['annio'] = tabla_anime['annio'].astype(int)
     tabla_anime['temporada'] = tabla_anime['temporada'].astype(object)
-    tabla_anime['clasifificacion'] = tabla_anime['clasifificacion'].astype(object)
+    tabla_anime['clasificacion'] = tabla_anime['clasificacion'].astype(object)
     tabla_anime['duracion'] = tabla_anime['duracion'].astype(object)
     tabla_anime['sinopsis'] = tabla_anime['sinopsis'].astype(object)
     tabla_anime['anime_rank'] = tabla_anime['anime_rank'].astype(int)
-    print(f'\n--- DataFrame {"Anime"} despues del cambio ---')
-    print(tabla_anime.info())
 
     #Tabla genero
     tabla_genero = lista_df[1]
-    print(f'\n--- DataFrame {"tabla_genero"} antes del cambio ---')
-    print(tabla_genero.info())
     tabla_genero['genero_id'] = tabla_genero['genero_id'].astype(int)
     tabla_genero['nombre_genero'] = tabla_genero['nombre_genero'].astype(object)
-    print(f'\n--- DataFrame {"tabla_genero"} despues del cambio ---')
-    print(tabla_genero.info())
 
 
     #Tabla estudio
     tabla_estudio = lista_df[2]
-    print(f'\n--- DataFrame {"tabla_estudio"} antes del cambio ---')
-    print(tabla_estudio.info())
     tabla_estudio['mal_id'] = tabla_estudio['mal_id'].astype(int)
     tabla_estudio['nombre_studio'] = tabla_estudio['nombre_studio'].astype(object)
     tabla_estudio['favoritos'] = tabla_estudio['favoritos'].astype(int)
-    tabla_estudio['establecido'] = (pd.to_datetime(tabla_estudio['establecido'], errors='coerce').dt.date.astype(str))
-    tabla_estudio['establecido'] = (
-    tabla_estudio['establecido']
-    .replace('NaT', None) # Reemplaza la cadena 'NaT' con el objeto Python None
-)
-    print(f'\n--- DataFrame {"tabla_estudio"} despues del cambio ---')
+    tabla_estudio['establecido'] = pd.to_datetime(tabla_estudio['establecido'], format='%Y-%m-%d')
+    # tabla_estudio['establecido'] = tabla_estudio['establecido'].dt.strftime('%Y-%m-%d')
     print(tabla_estudio.info())
 
     #Tabla de popularidad
     tabla_popularidad = lista_df[3]
-    print(f'\n--- DataFrame {"tabla_popularidad"} antes del cambio ---')
-    print(tabla_popularidad.info())
     tabla_popularidad['mal_id'] = tabla_popularidad['mal_id'].astype(int)
     tabla_popularidad['score'] = tabla_popularidad['score'].astype(float)
     tabla_popularidad['scored_by'] = tabla_popularidad['scored_by'].astype(int)
     tabla_popularidad['miembros'] = tabla_popularidad['miembros'].astype(int)
     tabla_popularidad['favoritos'] = tabla_popularidad['favoritos'].astype(int)
     tabla_popularidad['popularidad'] = tabla_popularidad['popularidad'].astype(int)
-    print(f'\n--- DataFrame {"tabla_popularidad"} despues del cambio ---')
-    print(tabla_popularidad.info())
 
     #Tabla anime_genero
     tabla_anime_genero = lista_df[4]
-    print(f'\n--- DataFrame {"tabla_anime_genero"} antes del cambio ---')
-    print(tabla_anime.info())
     tabla_anime_genero['mal_id'] = tabla_anime_genero['mal_id'].astype(int)
     tabla_anime_genero['genero_id'] = tabla_anime_genero['genero_id'].astype(int)
-    print(f'\n--- DataFrame {"tabla_anime_genero"} despues del cambio ---')
-    print(tabla_anime_genero.info())
 
     #Tabla anime_estudio
     tabla_anime_estudio = lista_df[5]
-    print(f'\n--- DataFrame {"anime_estudios"} antes del cambio ---')
-    print(tabla_anime_estudio.info())
     tabla_anime_estudio['mal_id'] = tabla_anime_estudio['mal_id'].astype(int)
     tabla_anime_estudio['studio_id'] = tabla_anime_estudio['studio_id'].astype(int)
-    print(f'\n--- DataFrame {"anime_estudios"} despues del cambio ---')
-    print(tabla_anime_estudio.info())
 
     lista_df_tipo_correcto.append(tabla_anime)
     lista_df_tipo_correcto.append(tabla_genero)
@@ -115,6 +94,3 @@ def cambio_tipo_dato(lista_df):
     lista_df_tipo_correcto.append(tabla_anime_estudio)
 
     return lista_df_tipo_correcto
-
-
-# En la función cambio_tipo_dato se puede eliminar los print antes y despues del cambio solo, no son tan necesarios.
