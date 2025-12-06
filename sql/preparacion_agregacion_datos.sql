ea3fa9c7ba10e1f812eeeb27240b77d0bfb93325
Said Mariano <smariano170@gmail.com>
2025-12-06 16:17:45 -0600

    refactor: Modificación a consulta para obtención de promedio general por año

--- sql/preparacion_agregacion_datos.sql
+++ sql/preparacion_agregacion_datos.sql
@@ -10,12 +10,16 @@ FROM
 	popularidad pop
 JOIN 
 	animes ani ON pop.mal_id = ani.mal_id
+WHERE
+	score < 9999
+AND
+	annio > 0
 GROUP BY 
 	ani.annio
 ORDER BY
 	annio DESC;
 
--- En los últimos años(20 años) el promedio de score por año es de 6, lo que nos puede dar a entender de que no todos los animes en ese año fueron bien resibidos.
+-- En los últimos años(20 años) el promedio de score por año es de 6, lo que nos puede dar a entender de que no todos los animes en ese esos años fueron bien recibidos.
 
 -- 2.-Conocer la oferta de anime por año y temporada
 SELECT
